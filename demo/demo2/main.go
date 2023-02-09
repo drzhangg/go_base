abb109c639478f95e8fe9c3cbdec4dde7021eae9
drzhangg <654014730@qq.com>
2023-02-09 21:10:52 +0800

    康复训练

--- demo/demo2/main.go
+++ demo/demo2/main.go
@@ -3,12 +3,32 @@ package main
 import "fmt"
 
 func main() {
-	nums := []int{1,3,2,6,4,5,3}
-	fmt.Println(sort(nums))
+	numbers := []int{1,3,2,6,4,5}
+	//fmt.Println(sort(nums))
+
+	//numbers := []int{1, 2, 3, 4, 5}
+	target := 8
+	result := findPairsWithSum(numbers, target)
+	fmt.Println(result)
 }
 
 func allSum() [][]int {
 	return nil
+}
+
+func findPairsWithSum(numbers []int, target int) [][]int {
+	result := [][]int{}
+	// 创建哈希表，用于存储第一个整数
+	m := make(map[int]bool)
+	for i := 0; i < len(numbers); i++ {
+		complement := target - numbers[i]
+		// 判断是否存在第二个整数，使得它们的和为指定值
+		if m[complement] {
+			result = append(result, []int{complement, numbers[i]})
+		}
+		m[numbers[i]] = true
+	}
+	return result
 }
 
 // 排序
---
+++ demo/makeandnew/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	a := make(map[string]string)
+	a["name"] = "jerry"
+	fmt.Println(a)
+
+
+	//b := new(map[string]string)
+	//b["name"] = "jerry"
+	//fmt.Println(b)
+}
