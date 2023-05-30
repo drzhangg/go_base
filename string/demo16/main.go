356bf71e6cf63dfe3df8dcde758fae98104542a9
drzhangg <654014730@qq.com>
2023-05-30 15:31:03 +0800

    expr

---
+++ string/demo16/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+)
+
+func main() {
+	errMsg := `<div class=error><img src='/static/7abb227e/images/none.gif' height=16 width=1>A job already exists with the name ‘demo1’</div>`
+
+	// 定义正则表达式模式
+	pattern := `>[^<]+<`
+
+	// 编译正则表达式
+	reg := regexp.MustCompile(pattern)
+
+	// 提取错误消息
+	match := reg.FindString(errMsg)
+	if match != "" {
+		errorMessage := match[1:len(match)-1]
+		fmt.Println(errorMessage)
+	} else {
+		fmt.Println("Failed to extract error message")
+	}
+}
