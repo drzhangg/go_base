ddcfa05b7d15101a3b82085edc625283db18edf2
drzhangg <654014730@qq.com>
2023-07-12 07:36:49 +0800

    new

--- algorithm/lc/35/main.go
+++ algorithm/lc/35/main.go
@@ -1,16 +1,16 @@
 package main
 
 func searchInsert(nums []int, target int) int {
-	l,r := 0,len(nums)-1
+	l, r := 0, len(nums)-1
 
 	for l <= r {
-		mid := l + (r-l) /2
-		if nums[mid] == target{
+		mid := (l + r) / 2
+		if nums[mid] == target {
 			return mid
-		}else if nums[mid] < target{
-			l = mid+1
-		}else {
-			r = mid-1
+		} else if nums[mid] < target {
+			l = mid + 1
+		} else {
+			r = mid - 1
 		}
 	}
 
