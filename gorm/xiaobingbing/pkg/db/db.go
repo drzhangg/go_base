1c28db89e2c41d71bf63cc07fc97f7c20032c75c
drzhangg <654014730@qq.com>
2024-01-09 14:26:12 +0800

    demo

--- gorm/xiaobingbing/pkg/db/db.go
+++ gorm/xiaobingbing/pkg/db/db.go
@@ -7,7 +7,7 @@ import (
 )
 
 func NewDataBase() *gorm.DB {
-	dsn := "root:root@tcp(150.158.87.137:3306)/daota?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:root@tcp(xxx:3306)/daota?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		fmt.Println(err)
