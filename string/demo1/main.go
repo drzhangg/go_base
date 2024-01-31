4d4b93d40c6c74bcd6f6675ceacec22b9547a8a8
zhangjunhui <zhangjunhui@sensoro.com>
2024-01-31 16:58:03 +0800

    demo

--- string/demo1/main.go
+++ string/demo1/main.go
@@ -1,20 +1,34 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
+import "fmt"
 
 func main() {
 	// u001b[1;32mhellou001b[m  u001b[1;34mlogsu001b[m
-	a := "u001b[1;32mhellou001b[m  u001b[1;34mlogsu001b[m"
+	//a := "u001b[1;32mhellou001b[m  u001b[1;34mlogsu001b[m"
+	//
+	//b := strings.ReplaceAll(a,"u001b","%c")
+	//
+	//ss := []interface{}{}
+	//for i := 0; i < strings.Count(a,"u001b"); i++ {
+	//	ss = append(ss, 0x1B)
+	//}
+	//fmt.Printf(b,ss...)
 
-	b := strings.ReplaceAll(a,"u001b","%c")
-	
-	ss := []interface{}{}
-	for i := 0; i < strings.Count(a,"u001b"); i++ {
-		ss = append(ss, 0x1B)
+	flag := false
+
+	dbName := "test"
+	dsn := fmt.Sprintf("clickhouse://%s", "127.0.0.1:8123")
+
+	if dbName != "" {
+		dsn = fmt.Sprintf(dsn+"?database=%s", dbName)
 	}
-	fmt.Printf(b,ss...)
 
+	if flag {
+		dsn = fmt.Sprintf(dsn+"&username=%s&password=%s", "admin","admin1")
+	}
+
+
+
+
+	fmt.Println(dsn)
 }
