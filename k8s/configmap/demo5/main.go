758450a61bceb7e1a54193641c338410fdec9498
drzhangg <654014730@qq.com>
2024-05-08 16:53:33 +0800

    test

--- k8s/configmap/demo5/main.go
+++ k8s/configmap/demo5/main.go
@@ -5,11 +5,14 @@ import (
 	"flag"
 	"fmt"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/yaml"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 	"os"
 	"path/filepath"
 )
+
+
 
 func main() {
 
@@ -30,45 +33,36 @@ func main() {
 	}
 
 	ns := "sre"
-	name := "wakanada-role-config"
-
+	name := "wakanda-role-config"
 
 	cm ,err := clientset.CoreV1().ConfigMaps(ns).Get(context.Background(),name,v1.GetOptions{})
 	if err != nil {
 		fmt.Println("cm err:",err)
 	}
 
-	//fmt.Println(cm.Data)
 
-	//user := cm.Data["user"]
+	conf := cm.Data["role.conf"]
+	fmt.Println("role::",conf)
+	var auth Roles
 
-	//m := make(map[string]map[string][]string)
-	for k, _ := range cm.Data{
-		fmt.Println("k::",k)
-
-		fmt.Println("k1::",cm.Data[k])
-
-
-		//for _,val1 := range cm.Data[k]{
-		//	fmt.Println("val::",string(val1))
-		//}
-
-		//for _, val1 := range cm.Data[val]{
-		//	arr1 := strings.Split(cm.Data[val][val1],":")
-		//	verbs := strings.Split(arr1[1],",")
-		//}
+	err = yaml.Unmarshal([]byte(conf),&auth)
+	if err != nil {
+		fmt.Println("unmarsharl err:",err)
 	}
 
-	//fmt.Println(user)
+	fmt.Println("auth::",auth)
+}
 
-	//fmt.Println(reflect.TypeOf(user))
+type Roles struct {
+	Authorities []Authority
+}
 
+type Authority struct {
+	Role string
+	Rules []Rule
+}
 
-	//
-	//fmt.Println(verbs)
-	//
-	//
-	//m[arr1[0]] = append([]string{}, verbs...)
-	//
-	//fmt.Println(m)
+type Rule struct {
+	Resource string
+	Verbs map[string]bool
 }
